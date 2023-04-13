4f0817ee2aaa1480323b7d5b72f25a78c614784c
Drewthurm21 <drewthurm21@gmail.com>
2023-04-12 23:25:25 -0500

    adding styles to the user form

--- src/pages/forms/loginForm.js
+++ src/pages/forms/loginForm.js
@@ -27,19 +27,21 @@ export default function LoginForm(props) {
 
 
   return (
-    <div className="container mx-auto my-auto">
+    <div className="container w-1/2 px-10 py-10 border-solid form-border text-center">
       <h1>Form Header</h1>
       {userSelected ? <div>Logged in</div> : <div>Not Logged in</div>}
-      <form onSubmit={checkUser}>
-        <label
+      <form className='px-5 py-5 flex flex-col' onSubmit={checkUser}>
+        <label className='px-5 py-3'
           htmlFor='userName'
-        > label:
-          <input id='userName' type='text' value={userName} placeholder="input" onChange={e => setUserName(e.target.value)}></input>
+        > .userName :
+          <input
+            className='px-5' id='userName' type='text' value={userName} placeholder="input" onChange={e => setUserName(e.target.value)}></input>
         </label>
-        <label
+        <label className='px-5 py-3'
           htmlFor='password'
-        > label:
-          <input id='password' type='text' value={password} placeholder="input" onChange={e => setPassword(e.target.value)}></input>
+        > .password :
+          <input
+            className='px-5' id='password' type='text' value={password} placeholder="input" onChange={e => setPassword(e.target.value)}></input>
         </label>
         <button>CLICK</button>
       </form>
